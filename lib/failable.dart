8690a6418e7dfde9d56d3b3764d9faf8f04ede13
Kerrely <kollert.honzik@gmail.com>
2023-02-04 17:15:12 +0100

    Project upload

---
+++ lib/failable.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library failable;
+
+export 'src/failable_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
